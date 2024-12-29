b7f138fe677fe844b72ca1e7f9e46da81a46e362
gzbotii <gzbotii@gmail.com>
2024-12-29 21:19:49 +0200

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,11 @@ function Footer() {
   return (
     <footer className="container mx-auto py-4 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Created by
-        <span className="font-bold"> gzbotii </span> with{" "}
+        Made by
+        <span className="font-medium"> gzbotii </span> with{" "}
         <span className="text-gradient font-medium"> React </span> &{" "}
-        <span className="text-gradient font-medium"> Coffee </span>{" "}
+        <span className="text-gradient font-medium"> Tailwind </span>{" "}
+        <span>and occasional debugging tears</span>
       </p>{" "}
     </footer>
   );
