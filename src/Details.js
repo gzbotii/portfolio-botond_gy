29097c3adb07300486c8d6f1bce48857002ca8c8
gzbotii <gzbotii@gmail.com>
2024-12-29 14:21:34 +0200

    More details

--- src/Details.js
+++ src/Details.js
@@ -168,6 +168,6 @@ project lorem ipsum generator for dummy content`,
 
 // Enter your Contact Details here
 export const contactDetails = {
-  email: "hi@example.com",
-  phone: "+91 12345 67890",
+  email: "gzbotii@gmail.com",
+
 };
