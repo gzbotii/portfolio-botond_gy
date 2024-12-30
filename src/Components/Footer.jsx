85e5dc2d956fd84a944b2a69fc6fdf9631049b43
gzbotii <gzbotii@gmail.com>
2024-12-30 14:22:43 +0200

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <footer className="container mx-auto py-4 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
         Made by
-        <span className="font-medium"> gzbotii </span> with{" "}
-        <span className="text-gradient font-medium"> React </span> &{" "}
-        <span className="text-gradient font-medium"> Tailwind </span>{" "}
-        <span>and occasional debugging tears. Thanks for PwanMG.</span>
+        <span className="font-medium text-gradient"> gzbotii </span> with{" "}
+        <span className="font-medium"> React </span> &{" "}
+        <span className="font-medium"> Tailwind </span>{" "}
+        <span>and occasional debugging tears.</span>
       </p>{" "}
     </footer>
   );
