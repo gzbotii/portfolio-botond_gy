ea4f28481959b41377414d223885610a53d61a91
gzbotii <gzbotii@gmail.com>
2025-01-05 14:05:11 +0200

    Update about.cy.js

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -1,9 +1,9 @@
-describe("Home Component Tests", () => {
+describe("Work entries", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
-    it("should render 10 work experience entries", () => {
+    it("should render 11 work experiences", () => {
         cy.get('[data-test="work-experience"]')
             .find('> *') // Select all direct children
             .should('have.length', 11); // Assert that there are 10
