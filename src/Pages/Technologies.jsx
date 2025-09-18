8fa5ef2f3becc8c492f9fb68f43dde57a55eb8f1
gzbotii <gzbotii@gmail.com>
2025-09-18 17:31:43 +0300

    Update Technologies.jsx

--- src/Pages/Technologies.jsx
+++ src/Pages/Technologies.jsx
@@ -32,81 +32,31 @@ function Technologies() {
           Technologies I've been working with recently
         </p>
       </section>
-      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
-        <img src={react} title="React" alt="react" className="logo-uniform" />
-        <img
-          src={ts}
-          title="TypeScript"
-          alt="TypeScript"
-          className="logo-uniform"
-        />
-        <img src={vue} title="Vue.js" alt="vue.js" className="logo-uniform" />
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6 [&_img]:!w-full [&_img]:!h-full [&_img]:!object-fil [&_img]:flex [&_img]:items-center [&_img]:justify-center">
+        <img src={react} title="React" alt="react" />
+        <img src={ts} title="TypeScript" alt="TypeScript" />
+        <img src={vue} title="Vue.js" alt="vue.js" />
         {/* <img src={js} title="JS" alt="JavaScript" /> */}
-        <img src={gsap} title="GSAP" alt="GSAP" className="logo-uniform" />
-        <img
-          src={cypress}
-          title="Cypress"
-          alt="cypress"
-          className="logo-uniform"
-        />
-        <img
-          src={locust}
-          title="Locust"
-          alt="locust"
-          className="logo-uniform"
-        />
-        <img
-          src={tailwind}
-          title="Tailwind CSS"
-          alt="tailwind css"
-          className="logo-uniform"
-        />
-        <img
-          src={selenium}
-          title="Selenium"
-          alt="Selenium"
-          className="logo-uniform"
-        />
-        <img
-          src={python}
-          title="Python"
-          alt="Python"
-          className="logo-uniform"
-        />
+        <img src={gsap} title="GSAP" alt="GSAP" />
+        <img src={cypress} title="Cypress" alt="cypress" />
+        <img src={locust} title="Locust" alt="locust" />
+        <img src={tailwind} title="Tailwind CSS" alt="tailwind css" />
+        <img src={selenium} title="Selenium" alt="Selenium" />
+        <img src={python} title="Python" alt="Python" />
       </section>
       <section>
         <h5 className="text-2xl pt-10 md:pt-24 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           Tools
         </h5>
       </section>
-      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
-        <img
-          src={vscode}
-          title="Visual Studio Code"
-          alt=""
-          className="logo-uniform"
-        />
-        <img
-          src={github}
-          title="Github"
-          alt="Github"
-          className="logo-uniform"
-        />
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6 [&_img]:!w-full [&_img]:!h-full [&_img]:!object-fill  [&_img]:flex [&_img]:items-center [&_img]:justify-center">
+        <img src={vscode} title="Visual Studio Code" alt="" />
+        <img src={github} title="Github" alt="Github" />
         {/* <img src={chatgpt} title="ChatGPT" alt="ChatGPT" /> */}
-        <img src={figma} title="Figma" alt="Figma" className="logo-uniform" />
-        <img src={jira} title="Jira" alt="Jira" className="logo-uniform" />
-        <img
-          src={postman}
-          title="Postman"
-          alt="Postman"
-          className="logo-uniform"
-        />
-        <img
-          src={gsheet}
-          title="Google Sheet"
-          alt="Google Sheet"
-          className="logo-uniform"
-        />
+        <img src={figma} title="Figma" alt="Figma" />
+        <img src={jira} title="Jira" alt="Jira" />
+        <img src={postman} title="Postman" alt="Postman" />
+        <img src={gsheet} title="Google Sheet" alt="Google Sheet" />
       </section>
     </main>
   );
